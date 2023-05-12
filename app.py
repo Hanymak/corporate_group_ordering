b39d6304dec75b09cb7662ced864d50ad204e2b8
Hanymak <38921397+Hanymak@users.noreply.github.com>
2023-05-12 23:33:05 +0000

    Fully working Transactions for users and transaction history tracking page

--- app.py
+++ app.py
@@ -1,41 +1,88 @@
-from flask import Flask, render_template, jsonify
-from database import engine
+from flask import Flask, render_template, jsonify, request, redirect
+from database import engine, add_transfered_balance_to_user_db, balance_recharge_to_user_db
 from sqlalchemy import text
 
 app = Flask(__name__)
 
 
-
 def load_orders_from_db():
   with engine.connect() as conn:
-    result = conn.execution_options(stream_results=True).execute(text("select * from orders"))
+    result = conn.execution_options(stream_results=True).execute(
+      text("select * from orders"))
     result_all = result.all()
-    column_names = result.keys() 
+    column_names = result.keys()
     orders = []
     for idx, r in enumerate(result_all):
       orders.append(dict(zip(column_names, r)))
-  
 
   return orders
 
+
 def load_users_from_db():
   with engine.connect() as conn:
-    result = conn.execution_options(stream_results=True).execute(text("select * from users"))
+    result = conn.execution_options(stream_results=True).execute(
+      text("select * from users"))
     result_all = result.all()
-    column_names = result.keys() 
+    column_names = result.keys()
     users = []
     for idx, r in enumerate(result_all):
       users.append(dict(zip(column_names, r)))
   return users
 
+
+def load_transactions_from_db():
+  with engine.connect() as conn:
+    result = conn.execution_options(stream_results=True).execute(
+      text("select * from transactions"))
+    result_all = result.all()
+    column_names = result.keys()
+    transactions = []
+    for idx, r in enumerate(result_all):
+      transactions.append(dict(zip(column_names, r)))
+  return transactions
+
+
 @app.route("/")
 def html_redirect():
-  orders_from_db=load_orders_from_db()
-  users_from_db=load_users_from_db()
-  sheet_balance=sum(sub['user_balance'] for sub in users_from_db)
-  return render_template('home.html', orders=orders_from_db,
-                                      sheet_balance=sheet_balance,
-                                      users=users_from_db)
+  orders_from_db = load_orders_from_db()
+  users_from_db = load_users_from_db()
+  sheet_balance = sum(sub['user_balance'] for sub in users_from_db)
+  return render_template('home.html',
+                         orders=orders_from_db,
+                         sheet_balance=sheet_balance,
+                         users=users_from_db)
+
+
+#this function populates the transaction history
+@app.route("/transaction_history")
+def transaction_history():
+  transactions_from_db = load_transactions_from_db()
+  transactions_from_db_descending = sorted(
+    transactions_from_db,
+    key=lambda item: item['transaction_id'],
+    reverse=True)
+  return render_template('transaction_history.html',
+                         transactions=transactions_from_db_descending)
+
+
+@app.route("/money_transfer", methods=['post'])
+def transfer_money():
+  data = request.form
+  add_transfered_balance_to_user_db(data)
+
+  return redirect("/")
+
+
+@app.route("/balance_recharge", methods=['post'])
+def balance_recharge():
+  data = request.form
+  balance_recharge_to_user_db(data)
+
+  return redirect("/")
+
+  # return render_template('home.html', orders=orders_from_db,
+  #                                     sheet_balance=sheet_balance,
+  #                                     users=users_from_db)
 
 
 @app.route("/api/orders")
--- database.py
+++ database.py
@@ -1,30 +1,77 @@
 import os
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text, update, bindparam
+from datetime import datetime, timezone
+from pytz import timezone
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-      "ssl": {
-          "ssl_ca": "/etc/ssl/cert.pem"
-      }
-  }
-  
-)
-
-# def Convert(lst):
-#     print("order_len",len(lst))
-#     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-#     return res_dct
-# with engine.connect() as conn:
-#   result = conn.execution_options(stream_results=True).execute(text("select * from orders"))
-#   result_all = result.all()
-#   column_names = result.keys() 
-#   orders = []
-#   for idx, r in enumerate(result_all):
-#     orders.append(dict(zip(column_names, r)))
-  
-# print(orders)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
+def add_transfered_balance_to_user_db(transaction):
+  # getting the current time in UTC timezone
+  now_utc = datetime.now(timezone('UTC'))
+
+  # Converting to Asia/Kolkata time zone
+  now_cairo = now_utc.astimezone(timezone('Africa/Cairo'))
+  datetime_string = now_cairo.strftime("%d/%m/%Y %I:%M:%S")
+  #insert into DB for transactions table
+  with engine.connect() as conn:
+
+    query = text(
+      "INSERT INTO transactions (transaction_from_user,transaction_to_user,transaction_amount,transaction_reason,transaction_date) VALUES (:transaction_from_user,:transaction_to_user,:transaction_amount,:transaction_reason,:transaction_date)"
+    )
+
+    conn.execute(
+      query,
+      dict(transaction_from_user=transaction['from_user'],
+           transaction_to_user=transaction['to_user'],
+           transaction_amount=transaction['transfer_amount'],
+           transaction_reason=transaction['transfer_reason'],
+           transaction_date=datetime_string))
+    quer_user_dediction = text(
+      "UPDATE users SET user_balance=user_balance-:transaction_amount WHERE user_name = :transaction_from_user"
+    )
+    conn.execute(
+      quer_user_dediction,
+      dict(transaction_amount=transaction['transfer_amount'],
+           transaction_from_user=transaction['from_user']))
+    quer_user_addition = text(
+      "UPDATE users SET user_balance=user_balance+:transaction_amount WHERE user_name = :transaction_to_user"
+    )
+    conn.execute(
+      quer_user_addition,
+      dict(transaction_amount=transaction['transfer_amount'],
+           transaction_to_user=transaction['to_user']))
+
+
+def balance_recharge_to_user_db(transaction):
+  # getting the current time in UTC timezone
+  now_utc = datetime.now(timezone('UTC'))
+
+  # Converting to Asia/Kolkata time zone
+  now_cairo = now_utc.astimezone(timezone('Africa/Cairo'))
+  datetime_string = now_cairo.strftime("%d/%m/%Y %I:%M:%S")
+  #insert into DB for transactions table
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO transactions (transaction_from_user,transaction_to_user,transaction_amount,transaction_reason,transaction_date) VALUES (:transaction_from_user,:transaction_to_user,:transaction_amount,:transaction_reason,:transaction_date)"
+    )
+
+    conn.execute(
+      query,
+      dict(transaction_from_user="-",
+           transaction_to_user=transaction['to_user'],
+           transaction_amount=transaction['transfer_amount'],
+           transaction_reason="Balance Recharge",
+           transaction_date=datetime_string))
+    quer_user_addition = text(
+      "UPDATE users SET user_balance=user_balance+:transaction_amount WHERE user_name = :transaction_to_user"
+    )
+    conn.execute(
+      quer_user_addition,
+      dict(transaction_amount=transaction['transfer_amount'],
+           transaction_to_user=transaction['to_user']))
